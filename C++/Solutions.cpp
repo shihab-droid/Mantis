cf843d1f45c81c8ceb863b9c2ffcb77b1ce22fd0
shihab-droid <muhamshihab@gmail.com>
2022-04-15 19:48:52 +0600

    Initializing

---
+++ C++/Solutions.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+        Team Mantis!
+        Solving problem 'A';
+    */
+
+    return 0;
+}
---
+++ C/Solutions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+       Team Mantis!
+        Solving problem 'A';
+    */
+    
+    return 0;
+}
